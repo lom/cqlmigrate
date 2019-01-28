b82073ee7036dd2be88a855aece362ed98383ecb
Solomatin Alexandr <a.solomatin@a1-systems.com>
2019-01-28 14:48:23 +0300

    migrate new guava

--- src/main/java/uk/sky/cqlmigrate/CqlFileParser.java
+++ src/main/java/uk/sky/cqlmigrate/CqlFileParser.java
@@ -99,7 +99,7 @@ class CqlFileParser {
         }
 
         private void findStatement(String original) throws IOException {
-            String line = CharMatcher.WHITESPACE.trimFrom(original);
+            String line = CharMatcher.whitespace().trimFrom(original);
 
             if (line.startsWith(CQL_COMMENT_DOUBLE_HYPEN) || line.startsWith(CQL_COMMENT_DOUBLE_SLASH) || line.isEmpty()) {
                 return;
@@ -112,7 +112,7 @@ class CqlFileParser {
 
             if (line.endsWith(CQL_STATEMENT_TERMINATOR)) {
                 curStmt.append(" ").append(line, 0, line.length() - 1);
-                statements.add(CharMatcher.WHITESPACE.trimFrom(curStmt.toString()));
+                statements.add(CharMatcher.whitespace().trimFrom(curStmt.toString()));
                 curState = State.IS_CLOSE_STMT;
                 process(original);
                 return;
@@ -122,7 +122,7 @@ class CqlFileParser {
             // and therefore is not a statement terminator
             if (CharMatcher.is(CQL_STATEMENT_STRING_DELIMITER).countIn(line) % 2 != 0) {
                 curState = State.IS_OPEN_VALUE_EXP;
-                curStmt.append(" ").append(CharMatcher.WHITESPACE.trimLeadingFrom(original));
+                curStmt.append(" ").append(CharMatcher.whitespace().trimLeadingFrom(original));
                 return;
             }
 
@@ -157,7 +157,7 @@ class CqlFileParser {
         }
 
         private void findMultilineComment(String original) {
-            if (CharMatcher.WHITESPACE.trimTrailingFrom(original).endsWith(CQL_MULTI_LINE_COMMENT_CLOSE))
+            if (CharMatcher.whitespace().trimTrailingFrom(original).endsWith(CQL_MULTI_LINE_COMMENT_CLOSE))
                 curState = State.FIND_EOS;
         }
 
